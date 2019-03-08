a63862c1fc81b931f4c19ae06c3ae4d63a340016
zhangwanyue <744404875@qq.com>
2019-03-08 16:14:07 +0800

    2019-3-8 finish VectorTest

--- exercise/VectorTest.cpp
+++ exercise/VectorTest.cpp
@@ -6,9 +6,16 @@
 #include<vector>
 using namespace std;
 
+void printVec(vector<int>& vec){
+    for(int i : vec){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+}
+
 int main(){
     //静态初始化
-    vector<int> vec = {0,1,2,3};
+    vector<int> vec = {0,1,2,3,4};
     vector<vector<int> > matrix = {{0,1,2},{3,4}};
     //下标访问
     cout<<matrix[1][1]<<" "<<vec[0]<<endl;
@@ -21,7 +28,7 @@ int main(){
         cout<< *iter <<" ";
     }
     cout<<endl;
-    for(auto& iter: vec){
+    for(auto iter : vec){
         cout<< iter <<" ";
     }
     cout<<endl;
@@ -38,8 +45,8 @@ int main(){
         }
     }
     cout<<endl;
-    for(auto& iter1 : matrix){
-        for(auto& iter2 : iter1){
+    for(auto iter1 : matrix){
+        for(auto iter2 : iter1){
             cout<< iter2 <<" ";
         }
     }
@@ -55,4 +62,23 @@ int main(){
     cout<<"size:"<<vec.size()<<" isEmpty:"<<vec.empty()<<" max_size:"<<vec.max_size()<<" capacity:"<<vec.capacity()<<endl;
     vec.shrink_to_fit();
     cout<<"size:"<<vec.size()<<" isEmpty:"<<vec.empty()<<" max_size:"<<vec.max_size()<<" capacity:"<<vec.capacity()<<endl;
+    //插入元素
+    printVec(vec);
+    vec.insert(vec.begin()+2, 10);
+    printVec(vec);
+    int arr[] = {101, 102, 103};
+    vec.insert(vec.begin()+3, arr, arr+3); //最后一个参数是指向要添加的最后一个元素的下一个元素的迭代器
+    printVec(vec);
+    vector<int> vec2 = {104, 105, 106};
+    vec.insert(vec.begin()+6, vec2.begin(), vec2.end());
+    printVec(vec);
+    //push & pop
+    vec.push_back(1000);
+    printVec(vec);
+    vec.pop_back();
+    printVec(vec);
+    vec.clear();
+    printVec(vec);
+
 }
+
