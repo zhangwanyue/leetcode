cfe98dcfa2ed60367028e7cc689763684f0a72d8
zhangwanyue <744404875@qq.com>
2019-04-08 09:21:15 +0800

    before merge

--- OfferCoding/p63_ReConstructBinaryTree.cpp
+++ OfferCoding/p63_ReConstructBinaryTree.cpp
@@ -93,6 +93,7 @@ list<list<int> > printTreeNodeFloors(TreeNode* root){
         }
         cout<<endl;
     }
+    return treeNodeList;
 }
 
 int main(){
@@ -112,7 +113,7 @@ int main(){
 //    node2->right = node6;
 //
 //    printTreeNodeFloors(root);
-
+//
     vector<int> pre = {1, 2, 4, 7, 3, 5, 6, 8};
     vector<int> vin = {4, 7, 2, 1, 5, 3, 8, 6};
     TreeNode* root = (new Solution)->reConstructBinaryTree(pre, vin);
