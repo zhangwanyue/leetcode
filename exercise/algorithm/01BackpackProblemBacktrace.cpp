c0b6ee75d234ae8c03bc97f9dfdf7ad8f1c7029e
zhangwanyue <744404875@qq.com>
2019-06-19 17:16:17 +0800

    finish 01BackpackProblemDP

--- exercise/algorithm/01BackpackProblemBacktrace.cpp
+++ exercise/algorithm/01BackpackProblemBacktrace.cpp
@@ -7,9 +7,9 @@
 using namespace std;
 
 const int NUM = 5;
-const int TOTALCAPACITY = 7;
+const int TOTALCAPACITY = 9;
 
-// 记忆数组
+// 记忆数组（使用了记忆数组之后，就相当于使用dp进行计算了）
 int dp[NUM+1][TOTALCAPACITY+1] = {}; // 全部初始化为0
 
 // (size, value)
@@ -18,11 +18,11 @@ vector<pair<int, int> > items = {make_pair(2, 3), make_pair(1, 2), make_pair(3, 
 // 统计递归调用次数
 int count = 0;
 
-// 表示将前i件物品（截止到第i个）放入一个容量为v的背包可以获得的最大价值
-int rec(int i, int remainCapacity){
+// 表示将前n件物品（截止到第n个）放入一个容量为v的背包可以获得的最大价值
+int rec(int n, int remainCapacity){
     count++;
 
-    if(i==0){
+    if(n==0){
         return 0;
     }
 
@@ -30,13 +30,13 @@ int rec(int i, int remainCapacity){
 //        return dp[i][remainCapacity];
 //    }
 
-    if(remainCapacity-items[i-1].first < 0){ // 放不下第i个物件
-        return rec(i-1, remainCapacity);
+    if(remainCapacity-items[n-1].first < 0){ // 放不下第n个物件
+        return rec(n-1, remainCapacity);
     }
 
-    // 计算放第i个物件或者不放第i个物件的最大值
-    dp[i][remainCapacity] = max(rec(i-1, remainCapacity), rec(i-1, remainCapacity-items[i-1].first) + items[i-1].second);
-    return dp[i][remainCapacity];
+    // 计算放第n个物件或者不放第n个物件的最大值
+    dp[n][remainCapacity] = max(rec(n-1, remainCapacity), rec(n-1, remainCapacity-items[n-1].first) + items[n-1].second);
+    return dp[n][remainCapacity];
 }
 
 
---
+++ exercise/algorithm/01BackpackProblemDP.cpp
@@ -0,0 +1,36 @@
+//
+// Created by vera on 19-6-19.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int NUM = 5;
+const int TOTALCAPACITY = 9;
+
+// (size, value)
+vector<pair<int, int> > items = {make_pair(2, 3), make_pair(1, 2), make_pair(3, 4), make_pair(2, 2), make_pair(3, 3)};
+
+// 表示将前n件物品（截止到第n个）放入一个容量为v的背包可以获得的最大价值
+int rec(int n, int capacity){
+    // dp数组
+    int dp[NUM+1][TOTALCAPACITY+1] = {}; // 全部初始化为0
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=capacity; j++){
+            if(j-items[i-1].first < 0){ // 放不下第i个物件
+                dp[i][j] = dp[i-1][j];
+            }else{
+                // 计算放第i个物件或者不放第i个物件的最大值
+                dp[i][j] = max(dp[i-1][j], dp[i-1][j-items[i-1].first] + items[i-1].second);
+            }
+        }
+    }
+    return dp[n][capacity];
+}
+
+
+int main(){
+    cout<<rec(NUM, TOTALCAPACITY)<<endl;
+}
